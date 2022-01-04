afaf22c77a676b5c435218618aa03ab929de68dd
Mads Tandrup <mads@maetzke-tandrup.dk>
2022-01-04 16:10:43 +0100

    start with max mem

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ cp -v ../spigot.jar .
 cp -v ../floodgate-spigot.jar plugins/
 cp -v ../Geyser-Spigot.jar plugins/
 
-java -Xms1G -Xmx2G -XX:+UseG1GC -Dcom.mojang.eula.agree=true -jar spigot.jar nogui
+java -Xms2G -Xmx2G -XX:+UseG1GC -Dcom.mojang.eula.agree=true -jar spigot.jar nogui
